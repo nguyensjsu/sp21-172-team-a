beeaa002c8c7f24f22e006b932ca50bbd5c433a9
devanshtandon00 <devansh.tandon@sjsu.edu>
2021-04-28 19:50:14 -0700

    cards.html to spring

--- frontend/spring-cashier/src/main/java/com/example/springcashier/GreetingController.java
+++ frontend/spring-cashier/src/main/java/com/example/springcashier/GreetingController.java
@@ -2,6 +2,8 @@ package com.example.springcashier;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,4 +16,13 @@ public class GreetingController {
 		return "greeting";
 	}
 
+	@RequestMapping(value = "/cards")
+   public String index() {
+      return "cards";
+   }
+
+   @RequestMapping(value = "/index")
+   public String index() {
+      return "cards";
+   }
 }
