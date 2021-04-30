33f6670086faece071a01430ead9dd58b77c5db1
JustineSD <justinedamolo@yahoo.com>
2021-04-29 21:27:27 -0700

    menu frontend and controller

---
+++ frontend/starbucksmenu2/src/main/java/com/example/starbucksmenu/StarbucksmenuApplication.java
@@ -0,0 +1,14 @@
+package com.example.starbucksmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksmenuApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksmenuApplication.class, args);
+	}
+
+}
