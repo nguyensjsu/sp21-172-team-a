85ed4d86509306bbdfc1f59beeda0eff5a9e5b3c
Angeline Lee <langeline@vmware.com>
2021-04-29 22:10:40 -0700

    remove justine's work and push to another branch

--- frontend/starbucksmenu2/src/main/java/com/example/starbucksmenu/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.starbucksmenu;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
-@Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
