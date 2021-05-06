564bb53887b825461977856dedb159fafd421a13
Angeline Lee <langeline@vmware.com>
2021-05-06 16:34:54 -0700

    delete cart testing

--- backend/spring-cashier/src/main/java/com/example/springcashier/SpringCashierApplication.java
+++ backend/spring-cashier/src/main/java/com/example/springcashier/SpringCashierApplication.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.example.springcashier;
 
 import org.springframework.boot.SpringApplication;
@@ -13,23 +11,3 @@ public class SpringCashierApplication {
     }
 
 }
-=======
-=======
->>>>>>> fad8dcfd292f7ea43b305f9888efba1e5fb647af
-package com.example.springcashier;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringCashierApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCashierApplication.class, args);
-	}
-
-}
-<<<<<<< HEAD
->>>>>>> combined menu and index.html
-=======
->>>>>>> fad8dcfd292f7ea43b305f9888efba1e5fb647af
