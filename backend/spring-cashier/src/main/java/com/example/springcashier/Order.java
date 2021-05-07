dc710a4d6d1e551be9edc035fcaedd26e0537efa
Angeline Lee <langeline@vmware.com>
2021-05-06 20:26:59 -0700

    switch device

--- backend/spring-cashier/src/main/java/com/example/springcashier/Order.java
+++ backend/spring-cashier/src/main/java/com/example/springcashier/Order.java
@@ -12,14 +12,13 @@ import lombok.*;
 @RequiredArgsConstructor
 public class Order {
 
-
     private String drink;
     private boolean milk;
     private String size;
     private int price;
 
     public Order(String drink, boolean milk, String size, int price){
-        this. drink = drink;
+        this.drink = drink;
         this.milk = milk;
         this.size = size;
         this.price = price;
