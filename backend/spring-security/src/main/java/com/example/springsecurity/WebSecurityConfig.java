0194bcc526ebcf4bd46b614a7785cb6b9fa525fc
Devansh Tandon <devansh.tandon@sjsu.edu>
2021-05-15 00:39:05 -0700

    updates to spring security

--- backend/spring-security/src/main/java/com/example/springsecurity/WebSecurityConfig.java
+++ backend/spring-security/src/main/java/com/example/springsecurity/WebSecurityConfig.java
@@ -2,12 +2,16 @@ package com.example.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -18,13 +22,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().formLogin()
                 .loginPage("/signIn").permitAll().and().logout().logoutSuccessUrl("/signIn").permitAll();
     }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin").password("admin1pass").roles("USER", "ADMIN").and()
+                .withUser("user1").password("user1pass").roles("USER").and()
+                .withUser("user2").password("user2pass").roles("USER").and()
+                .withUser("user3").password("user3pass").roles("USER");
+    }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public InMemoryUserDetailsManager getInMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager();
     }
 }
