55764bbc8606ba3f08a7bce6104120ea0c51d870
Angeline Lee <langeline@vmware.com>
2021-05-18 14:57:57 -0700

    rest api example in ordercontroller

--- backend/spring-starbucks-api/src/main/java/com/example/springstarbucksapi/StarbucksCardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springstarbucksapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface StarbucksCardRepository extends JpaRepository<StarbucksCard, Long> {
-    StarbucksCard findByCardNumber(String cardNumber);
-}
