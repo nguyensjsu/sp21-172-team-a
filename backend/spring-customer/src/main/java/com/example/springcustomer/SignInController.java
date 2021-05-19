a36283adce66ee1715381f64cda72cb525a6b36a
JustineSD <justinedamolo@yahoo.com>
2021-05-19 15:16:32 -0700

    separating cashier and customer apps

---
+++ backend/spring-customer/src/main/java/com/example/springcustomer/SignInController.java
@@ -0,0 +1,46 @@
+package com.example.springcustomer;
+import java.util.List;
+import java.util.HashMap;
+
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+@Controller
+@RequestMapping(value = {"/signIn"})
+public class SignInController {
+    @GetMapping
+    public String signIn( @ModelAttribute("signIn") Customer customer, Model model){
+        return "signIn";
+    }
+}
